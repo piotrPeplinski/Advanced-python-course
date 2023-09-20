ede56032203f5130d2df93eed2075c4927f7ebc5
Piotr Pepliński <p.peplaj@gmail.com>
2023-09-20 10:55:27 +0200

    got data for df

--- 3 - Web Crawling/script.py
+++ 3 - Web Crawling/script.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+rating_enum = {
+    'One': 1,
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5
+}
+
 with open('links.json') as file:
     links = json.load(file)
 
@@ -9,6 +17,12 @@ with open('links.json') as file:
 r = requests.get(links[0])
 
 soup = BeautifulSoup(r.text, 'html.parser')
-title = soup.find('h1')
 
-print(title.text)
+stock_el = soup.find(class_='availability').stripped_strings
+rating_key = soup.find(class_='star-rating').attrs['class'][1]
+
+title = soup.find('h1').text
+price = soup.find(class_='price_color').text[2:]
+stock = list(stock_el)[0].split(' ')[2][1:]
+rating = rating_enum[rating_key]
+
