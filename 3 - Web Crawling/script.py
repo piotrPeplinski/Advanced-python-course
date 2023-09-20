adca58b46f214e4e3abe145a6928ab3f68cb1923
Piotr Pepliński <p.peplaj@gmail.com>
2023-09-20 12:29:38 +0200

    update

--- 3 - Web Crawling/script.py
+++ 3 - Web Crawling/script.py
@@ -4,10 +4,25 @@ import matplotlib.pyplot as plt
 with open('data_frame.pickle', 'rb') as file:
     df = pickle.load(file)
 
-fig, ax = plt.subplots(figsize=(8, 5))
-ax.scatter(df['Rating'], df['Price'])
-ax.set_title('Rating relation to price',fontsize=14)
-ax.set_xlabel('Rating')
-ax.set_ylabel('Price')
-ax.grid(True)
+
+class Chart:
+    def __init__(self, x: list, y: list, title: str, x_label: str, y_label: str = 'Price'):
+        self.__x = x
+        self.__y = y
+        self.__x_label = x_label
+        self.__y_label = y_label
+        self.__title = title
+        self.generate_chart()
+
+    def generate_chart(self):
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.scatter(self.__x, self.__y)
+        ax.set_title(self.__title, fontsize=14)
+        ax.set_xlabel(self.__x_label)
+        ax.set_ylabel(self.__y_label)
+        ax.grid(True)
+
+
+chart = Chart(df['Rating'], df['Price'], 'Rating relation to price', 'Rating')
+
 plt.show()
