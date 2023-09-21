17576e22fb4c831e15fe73ee4b39f10b63d4cc7a
Piotr Pepliński <p.peplaj@gmail.com>
2023-09-21 15:35:11 +0200

    update

--- 5 - login system/script.py
+++ 5 - login system/script.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 
+credentials = {}
+
 root = tk.Tk()
 root.title('Authentication system')
 
@@ -19,11 +21,36 @@ username_entry.grid(row=0, column=1)
 password_label.grid(row=1, column=0)
 password_entry.grid(row=1, column=1)
 
-register_btn = tk.Button(frame, text='Register')
-login_btn = tk.Button(frame, text='Login')
+
+def register():
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if username in credentials:
+        messagebox.showerror('Registration Failed', 'Username already exists.')
+    else:
+        credentials[username] = password
+        messagebox.showinfo('Registration Successful', 'Account created successfully!')
+
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if username in credentials:
+        if credentials[username] == password:
+            messagebox.showinfo('Login successful','Login successful')
+        else:
+            messagebox.showerror('Login Failed', 'Incorrect password.')
+    else:
+        messagebox.showerror('Login Failed', 'No such user.')
+
+register_btn = tk.Button(frame, text='Register', command=register)
+login_btn = tk.Button(frame, text='Login', command=login)
 
 register_btn.grid(row=2, column=0)
 login_btn.grid(row=2, column=1)
+
+
 
 
 root.mainloop()
